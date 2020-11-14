b873c791668522d1ef0c7d2925958f4e48573545
Miltiadis Giannou <miltiadis.giannou@gmail.com>
2020-11-15 00:57:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/iwbcacademy/Register.java
@@ -0,0 +1,126 @@
+package com.example.iwbcacademy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class Register extends AppCompatActivity {
+    public static final String TAG = "TAG";
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+    //Variables
+    FirebaseAuth fAuth;
+    EditText phoneNumber, codeEnter;
+    Button nextBtn;
+    ProgressBar progressBar;
+    TextView state;
+    CountryCodePicker codePicker;
+    String verificationId;
+    PhoneAuthProvider.ForceResendingToken token;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+        phoneNumber = findViewById(R.id.phone);
+        codeEnter = findViewById(R.id.codeEnter);
+        progressBar = findViewById(R.id.progress_bar);
+        nextBtn = findViewById(R.id.nextBtn);
+        state = findViewById(R.id.state);
+        codePicker = findViewById(R.id.ccp);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!phoneNumber.getText().toString().isEmpty() && phoneNumber.getText().toString().length() == 10) {
+
+                    String phoneNum = "+" + codePicker.getSelectedCountryCode() + phoneNumber.getText().toString();
+                    Log.d(TAG, "onClick: Phone NO -> " + phoneNum);
+                    progressBar.setVisibility(View.VISIBLE);
+                    state.setText("Αποστολή OTP");
+                    state.setVisibility(View.VISIBLE);
+                    requestOTP(phoneNum);
+                } else {
+                    phoneNumber.setError("Μη έγκυρος Αρ. Τηλεφώνου");
+
+                }
+            }
+        });
+    }
+
+    private void requestOTP(String phoneNum) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(fAuth)
+                        .setPhoneNumber(phoneNum)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                progressBar.setVisibility(View.GONE);
+                state.setVisibility(View.GONE);
+                codeEnter.setVisibility(View.VISIBLE);
+                verificationId = s;
+                token = forceResendingToken;
+            }
+
+            @Override
+            public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
+                super.onCodeAutoRetrievalTimeOut(s);
+            }
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(Register.this, "Δεν δημιουργήθηκε λογ/μος" + e.getMessage() , Toast.LENGTH_SHORT).show();
+
+            }
+        };
+
+    }
+}
---
+++ app/src/main/java/com/example/iwbcacademy/SplashScreen.java
@@ -0,0 +1,69 @@
+package com.example.iwbcacademy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //This Line will hide the status bar from the screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Hooks
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Call next screen
+                Intent intent = new Intent(SplashScreen.this, com.example.iwbcacademy.Register.class);
+
+                //Attach all the elements those you want to animate in design
+                Pair[]  pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(image, "logo_image");
+                pairs[1] = new Pair<View,String>(logo, "logo_text");
+
+                //wrap the call in API level 21 or higher
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this,pairs);
+                    startActivity(intent,options.toBundle());
+                }
+
+            }
+        }, SPLASH_SCREEN);
+
+    }
+}
